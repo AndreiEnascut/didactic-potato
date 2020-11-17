58e0f399d06fb188d68138d0ea711549f991865d
Andrei Catalin Enascut <Andrei_Catalin.Enascut@lidl.ro>
2020-11-17 11:57:40 +0200

    add SFC format

---
+++ sfc/src/components/InfoTabs.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <ul>
+      <span
+        class="tabs"
+        :class="{ activeTab: selectedTab === tab }"
+        v-for="(tab) in tabs"
+        @click="selectedTab = tab"
+        :key="tab"
+        >{{ tab }}</span
+      >
+    </ul>
+
+    <div v-show="selectedTab === 'Shipping'">
+      <p>{{ shipping }}</p>
+    </div>
+
+    <div v-show="selectedTab === 'Details'">
+      <ul>
+        <li v-for="(detail, index) in details" :key="index">{{ detail }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InfoTabs",
+  props: {
+    shipping: {
+      required: true,
+    },
+    details: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      tabs: ["Shipping", "Details"],
+      selectedTab: "Shipping",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
